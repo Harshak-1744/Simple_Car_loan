48295e75718923811a5fd9f3af8aec814c3f21db
Thrf1nn <137567789+Thrf1nn@users.noreply.github.com>
2023-07-01 23:52:59 +0530

    Update Basic_Car_Rental_System.java

--- Basic_Car_Rental_System.java
+++ Basic_Car_Rental_System.java
@@ -1,37 +1,55 @@
 import java.util.Scanner;
-import java.lang.Math;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
-public class Basic_Car_Rental_System {
-    public static void main(String[] args) {
-        int max = 50;
-        int min = 10;
-        int total = 60;
+public class Simple_Car_Loan {
+    public static double calculateCarLoanEMI(double principal, double interestRate, int loanTerm) {
+        double r = interestRate / 12 / 100;  // Convert annual interest rate to monthly rate
+        int n = loanTerm * 12;  // Convert loan term from years to months
 
-        System.out.println("Welcome to BCR");
-        System.out.println("Per Day 50$ + 10$ deposite");
-        Scanner input = new Scanner(System.in);
-        System.out.println("DO YOU WANT TO RENT A CAR [Y or N]");
-        String response = input.next().toUpperCase();
-        if (response.equals("N")) {
-            System.out.println("You Selected No");
-        } else if (response.equals("Y")) {
-            System.out.println("PLEASE SELECT TYPE OF CAR ELECTRIC(E) or PETROL(P) ");
-            response = input.next().toUpperCase();
-            if (response.equals("E") || response.equals("P")) {
-                System.out.println("DO YOU HAVE ANY COUPAN [Y or N]");
-                response = input.next().toUpperCase();
-                if (response.equals("Y")) {
-                    int per = (int) (Math.random() * (max - min + 1) + min);
-                    System.out.println("Whoooo You got " + per + "%" + " " + "off from your order");
-                    Double discount = (per * 0.01);
-                    System.out.println("The Price is " + Math.abs(total * discount) + "$ Per day");
-                } else {
-                    System.out.println("Your Price is " + total + "$ Per day");
-                }
-            }
-
-            input.close();
-        }
+        double emi = (principal * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+        return emi;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        if (age >= 21 && age <= 60) {
+            System.out.println("Enter your income:");
+            int income = sc.nextInt();
+            if (income >= 25000) {
+                System.out.println("Enter your credit score:");
+                int creditScore = sc.nextInt();
+                if (creditScore >= 700) {
+                    System.out.println("You are eligible for a loan.");
+                } else {
+                    System.out.println("You are not eligible for a loan.");
+                }
+            } else {
+                System.out.println("You are not eligible for a loan.");
+            }
+        } else {
+            System.out.println("You are not eligible for a loan.");
+        }
+
+        System.out.println("EMI Calculator.....");
+
+        System.out.println("Enter Amount: ");
+        double principal = sc.nextDouble();
+        System.out.println("Enter interest rate: ");
+        double interestRate = sc.nextDouble();
+        System.out.println("Enter number of years: ");
+        int loanTerm = sc.nextInt();
+
+        double emi = calculateCarLoanEMI(principal, interestRate, loanTerm);
+        double roundedEMI = Math.round(emi * 100) / 100.0;
+
+        NumberFormat formatter = new DecimalFormat("#,##0.00");
+        String formattedEMI = formatter.format(roundedEMI);
+
+        System.out.println("EMI: " + formattedEMI);
+
+        sc.close();
+    }
 }
